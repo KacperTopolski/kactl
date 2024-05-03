bf3c22d9ba969f37dead30d7f8cf5c21c507436e
Simon Lindholm <simon.lindholm10@gmail.com>
2024-05-04 00:24:09 +0200

    Fix Factor.h inf-loop for n = 352523 * 352817
    Fixes #253.

--- stress-tests/number-theory/Factor.cpp
+++ stress-tests/number-theory/Factor.cpp
@@ -18,19 +18,25 @@ void assertValid(ull N, vector<ull> prFac){
     assert(cur == N);
 }
 int main() {
-    assert(factor(1).empty());
-    assert(factor(2) == vector<ull>{2});
-    assert((factor(2299) == vector<ull>{11, 19, 11}));
-    fwd(n,2,1e5) {
-        auto res = factor(n);
-        assertValid(n, res);
-        res = factor(n*ll(n));
-        assertValid(n*ll(n), res);
-    }
-    fwd(i,2,1e5) {
-        ull n = 1 + (uni()%(3ul<<61));
-        auto res = factor(n);
-        assertValid(n, res);
-    }
-    cout<<"Tests passed!"<<endl;
+	assert(factor(1).empty());
+	assert(factor(2) == vector<ull>{2});
+	assert((factor(2299) == vector<ull>{11, 19, 11}));
+	fwd(n,2,1e5) {
+		auto res = factor(n);
+		assertValid(n, res);
+		res = factor(n*ll(n));
+		assertValid(n*ll(n), res);
+	}
+	fwd(i,2,1e5) {
+		ull n = 1 + (uni()%(3ul<<61));
+		auto res = factor(n);
+		assertValid(n, res);
+	}
+	fwd(i,0,1e5) {
+		// max number that modmul can handle
+		ull n = 7268172458553106874 - i;
+		auto res = factor(n);
+		assertValid(n, res);
+	}
+	cout<<"Tests passed!"<<endl;
 }
