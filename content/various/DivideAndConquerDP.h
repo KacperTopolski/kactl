9e8f52e7e3573687cfba261f69ff1c242bfe47b1
Kacper Topolski <kacpertopolski@op.pl>
2024-07-24 00:45:11 +0200

    Change D&C DP to cp-algorithms

--- content/various/DivideAndConquerDP.h
+++ content/various/DivideAndConquerDP.h
@@ -1,28 +1,34 @@
 /**
- * Author: Simon Lindholm
- * License: CC0
- * Source: Codeforces
- * Description: Given $a[i] = \min_{lo(i) \le k < hi(i)}(f(i, k))$ where the (minimal) optimal $k$ increases with $i$, computes $a[i]$ for $i = L..R-1$.
- * Time: O((N + (hi-lo)) \log N)
- * Status: tested on http://codeforces.com/contest/321/problem/E
+ * Author: cp-algorithms
+ * Description: Solves reccurences of the form $dp(i, j) = \min_{0 \leq k \leq j} dp(i - 1, k - 1) + C(k, j)$ where optimal k increases with j
+ * Time: O(nm \log n)
  */
 #pragma once
 
-struct DP { // Modify at will:
-	int lo(int ind) { return 0; }
-	int hi(int ind) { return ind; }
-	ll f(int ind, int k) { return dp[ind][k]; }
-	void store(int ind, int k, ll v) { res[ind] = pii(k, v); }
+vector<ll> dp_before, dp_cur;
+ll C(int i, int j);
 
-	void rec(int L, int R, int LO, int HI) {
-		if (L >= R) return;
-		int mid = (L + R) >> 1;
-		pair<ll, int> best(LLONG_MAX, LO);
-		fwd(k, max(LO,lo(mid)), min(HI,hi(mid)))
-			best = min(best, make_pair(f(mid, k), k));
-		store(mid, best.second, best.first);
-		rec(L, mid, LO, best.second+1);
-		rec(mid+1, R, best.second, HI);
-	}
-	void solve(int L, int R) { rec(L, R, INT_MIN, INT_MAX); }
-};
+// compute dp_cur[l], ... dp_cur[r] (inclusive)
+void compute(int l, int r, int optl, int optr) {
+    if (l > r) return;
+    int mid = (l + r) >> 1;
+    pair<ll, int> best = {LLONG_MAX, -1};
+    for (int k = optl; k <= min(mid, optr); k++)
+        best = min(best, {(k ? dp_before[k - 1] : 0) + C(k, mid), k});
+    dp_cur[mid] = best.first;
+    int opt = best.second;
+    compute(l, mid - 1, optl, opt);
+    compute(mid + 1, r, opt, optr);
+}
+
+ll solve(int n, int m) {
+    dp_before.assign(n, 0);
+    dp_cur.assign(n, 0);
+    for (int i = 0; i < n; i++)
+        dp_before[i] = C(0, i);
+    for (int i = 1; i < m; i++) {
+        compute(0, n - 1, 0, n - 1);
+        dp_before = dp_cur;
+    }
+    return dp_before[n - 1];
+}
