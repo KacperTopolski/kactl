02e163baf18cb28bb42f289c3db30d155d79dd41
Kacper Topolski <kacpertopolski@op.pl>
2024-08-03 17:56:29 +0200

    Fix tests not working on WSL1

--- stress-tests/utilities/template.h
+++ stress-tests/utilities/template.h
@@ -13,10 +13,29 @@ typedef vector<int> vi;
 
 auto &operator<<(auto &out, pair<auto, auto> a) { return out << "(" << a.st << ", " << a.nd << ")"; }
 auto &operator<<(auto &out, auto a) {
-   out << "{";
-   for (auto b : a)
-      out << b << ", ";
-   return out << "}";
+    out << "{";
+    for (auto b : a)
+        out << b << ", ";
+    return out << "}";
 }
 void dump(auto... x) { ((cerr << x << ", "), ...) << '\n'; }
 #define debug(x...) cerr << __LINE__ << " [" #x "]: ", dump(x)
+
+// WSL1 is broken, remove once nobody in the team uses WSL1
+#if __has_include (<fpu_control.h>)
+#include <fpu_control.h>
+namespace wsl {
+auto is_wsl = [](){
+    ifstream stream("/proc/version");
+    string str;
+    getline(stream, str);
+    for (char &ch : str)
+        ch = tolower((unsigned char) ch);
+    if (str.find("microsoft") == string::npos)
+        return false;
+    unsigned short Cw = 0x37f;
+    _FPU_SETCW(Cw);
+    return true;
+}();
+}
+#endif
